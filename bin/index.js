ee1c411f5413a603c662fec4b9f26ba0f4300bf3
almeidabbm <almeidabatistam@gmail.com>
2022-05-29 02:17:31 +0100

    Fix paths. Run in CLI

---
+++ bin/index.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+const { program } = require("commander");
+const path = require("path");
+const { parseInt } = require("../src/custom-parsers");
+const runWorkers = require("../src/run-workers");
+
+program
+  .requiredOption("-f, --file <string>", "Path of the script to run")
+  .option("-w, --workers <number>", "Number of workers to run", parseInt)
+  .option(
+    "-i, --iterations <number>",
+    "Number of iteration to be run by each worker",
+    parseInt
+  );
+
+program.parse();
+
+const opts = program.opts();
+
+runWorkers(
+  path.join(process.cwd(), opts.file),
+  Number(opts.workers),
+  Number(opts.iterations)
+);
